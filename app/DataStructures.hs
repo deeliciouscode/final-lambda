9b3a18ebd36ac3af7a08b29ffbbe0d7e6b9b62e8
Paul Felkner <p.felkner@campus.lmu.de>
2022-03-27 21:44:09 +0200

    added Quests; not yet final

---
+++ app/DataStructures.hs
@@ -0,0 +1,63 @@
+module DataStructures where
+import Data.Map as Map
+
+-- each player has list of QuestInfo, representing the quests he has currently "assigned" to him
+data QuestInfo = QuestInfo
+    { index :: Int
+    , contractor :: Contractor
+    , quest :: Quest
+    , state :: QuestState
+    }
+    deriving(Show)
+
+data Quest = Quest 
+    { qtype :: QuestType
+    , progress :: QuestProgress
+    , reward :: QuestReward
+    , dialogue :: Dialogue
+    }
+    deriving(Show)
+
+data QuestType 
+    = Spend
+    | Fetch
+    | Kill
+    | Discover
+    | Collect
+    deriving(Show, Eq)
+
+data QuestState
+    = OnGoing
+    | Done
+    | RewardReceived
+    deriving(Show, Eq)
+
+data QuestProgress
+    = Counter { current :: Int, target :: Int}
+    | CountAndCond { condition :: Bool, current :: Int, target :: Int}
+    | Flag Bool
+    deriving(Show)
+
+data QuestReward = Item | StatPoint Int deriving Show
+
+data Action = BeginQuestline | Accept | MarkAsDone | KeepGoing deriving Show
+
+type PlayerQuests = [QuestInfo]
+
+data Item = Equipment | Weapon | QuestItem | Pebble
+
+-- type StatPoint = Int
+
+type Contractor = String
+
+type Questline = [Quest]
+
+data Dialogue = Dialogue
+    { proposition :: String
+    , response :: Response
+    , end :: String
+    }
+    deriving Show
+
+-- data Response = Option1 String | Option2 String deriving Show
+type Response = String
